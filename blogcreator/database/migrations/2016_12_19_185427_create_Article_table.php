9fc8a6c741f3d30d47177e51a85d373c860fd0db
Ruben C <rubencherbit@gmail.com>
2016-12-19 20:09:55 +0100

    add blog id to article migration

--- blogcreator/database/migrations/2016_12_19_185427_create_Article_table.php
+++ blogcreator/database/migrations/2016_12_19_185427_create_Article_table.php
@@ -15,6 +15,7 @@ class CreateArticleTable extends Migration
     {
         Schema::create('article', function(Blueprint $table) {
             $table->increments('id');
+            $table->integer('blog_id');
             $table->text('title');
             $table->text('description');
             $table->text('content');
