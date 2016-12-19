41e87ed126fa25b1ece363a7954fda692f9fff75
Ruben C <rubencherbit@gmail.com>
2016-12-19 21:21:23 +0100

    Blog controller and view for crud generator

--- blogcreator/app/Http/Controllers/BlogController.php
+++ blogcreator/app/Http/Controllers/BlogController.php
@@ -2,83 +2,140 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use App\Blog;
 use Illuminate\Http\Request;
+use Session;
 
 class BlogController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\View\View
      */
     public function index()
     {
-        //
+        $blog = Blog::paginate(25);
+
+        return view('Blog.blog.index', compact('blog'));
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\View\View
      */
     public function create()
     {
-        //
+        return view('Blog.blog.create');
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
      */
     public function store(Request $request)
     {
-        //
+
+        $requestData = $request->all();
+
+
+        if ($request->hasFile('banner')) {
+            $uploadPath = public_path('/uploads/');
+
+            $extension = $request->file('banner')->getClientOriginalExtension();
+            $fileName = rand(11111, 99999) . '.' . $extension;
+
+            $request->file('banner')->move($uploadPath, $fileName);
+            $requestData['banner'] = $fileName;
+        }
+
+        Blog::create($requestData);
+
+        Session::flash('flash_message', 'Blog added!');
+
+        return redirect('blog/blog');
     }
 
     /**
      * Display the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     *
+     * @return \Illuminate\View\View
      */
     public function show($id)
     {
-        //
+        $blog = Blog::findOrFail($id);
+
+        return view('Blog.blog.show', compact('blog'));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     *
+     * @return \Illuminate\View\View
      */
     public function edit($id)
     {
-        //
+        $blog = Blog::findOrFail($id);
+
+        return view('Blog.blog.edit', compact('blog'));
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
      */
-    public function update(Request $request, $id)
+    public function update($id, Request $request)
     {
-        //
+
+        $requestData = $request->all();
+
+
+        if ($request->hasFile('banner')) {
+            $uploadPath = public_path('/uploads/');
+
+            $extension = $request->file('banner')->getClientOriginalExtension();
+            $fileName = rand(11111, 99999) . '.' . $extension;
+
+            $request->file('banner')->move($uploadPath, $fileName);
+            $requestData['banner'] = $fileName;
+        }
+
+        $blog = Blog::findOrFail($id);
+        $blog->update($requestData);
+
+        Session::flash('flash_message', 'Blog updated!');
+
+        return redirect('blog/blog');
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
      */
     public function destroy($id)
     {
-        //
+        Blog::destroy($id);
+
+        Session::flash('flash_message', 'Blog deleted!');
+
+        return redirect('blog/blog');
     }
 }
---
+++ blogcreator/resources/views/Blog/blog/show.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Blog {{ $blog->id }}</div>
+                    <div class="panel-body">
+
+                        <a href="{{ url('blog/blog/' . $blog->id . '/edit') }}" class="btn btn-primary btn-xs" title="Edit Blog"><span class="glyphicon glyphicon-pencil" aria-hidden="true"/></a>
+                        {!! Form::open([
+                            'method'=>'DELETE',
+                            'url' => ['blog/blog', $blog->id],
+                            'style' => 'display:inline'
+                        ]) !!}
+                            {!! Form::button('<span class="glyphicon glyphicon-trash" aria-hidden="true"/>', array(
+                                    'type' => 'submit',
+                                    'class' => 'btn btn-danger btn-xs',
+                                    'title' => 'Delete Blog',
+                                    'onclick'=>'return confirm("Confirm delete?")'
+                            ))!!}
+                        {!! Form::close() !!}
+                        <br/>
+                        <br/>
+
+                        <div class="table-responsive">
+                            <table class="table table-borderless">
+                                <tbody>
+                                    <tr>
+                                        <th>ID</th><td>{{ $blog->id }}</td>
+                                    </tr>
+                                    <tr><th> Title </th><td> {{ $blog->title }} </td></tr><tr><th> Description </th><td> {{ $blog->description }} </td></tr><tr><th> Banner </th><td> {{ $blog->banner }} </td></tr>
+                                </tbody>
+                            </table>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
