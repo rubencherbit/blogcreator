b53da9360e6d9976b11f4c5fe20fac72d3243c16
laidet_r <rodolphe.laidet@epitech.eu>
2016-12-19 22:11:01 +0100

    [] Adapt the register form the same way [#]

--- blogcreator/resources/views/auth/register.blade.php
+++ blogcreator/resources/views/auth/register.blade.php
@@ -1,36 +1,59 @@
-@extends('layouts.master')
+@extends('layouts.app')
 
-@section('title', 'Register')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Create New Blog</div>
+                    <div class="panel-body">
 
-@section('main-content')
-@parent
-<h2>Register</h2>
+                        @if ($errors->any())
+                            <ul class="alert alert-danger">
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
+                        @endif
 
-<div>
-@if (count($errors) > 0)
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-@endif
+                        {!! Form::open(['route' => 'register', 'class' => 'form-horizontal']) !!}
+                        <div class="form-group {{ $errors->has('name') ? 'has-error' : ''}}">
+                            {!! Form::label('name', 'Name', ['class' => 'col-md-4 control-label']) !!}
+                            <div class="col-md-6">
+                                {!! Form::text('name', null, ['class' => 'form-control']) !!}
+                                {!! $errors->first('name', '<p class="help-block">:message</p>') !!}
+                            </div>
+                        </div><div class="form-group {{ $errors->has('email') ? 'has-error' : ''}}">
+                            {!! Form::label('email', 'Email', ['class' => 'col-md-4 control-label']) !!}
+                            <div class="col-md-6">
+                                {!! Form::text('email', null, ['class' => 'form-control']) !!}
+                                {!! $errors->first('email', '<p class="help-block">:message</p>') !!}
+                            </div>
+                        </div><div class="form-group {{ $errors->has('password') ? 'has-error' : ''}}">
+                            {!! Form::label('password', 'Password', ['class' => 'col-md-4 control-label']) !!}
+                            <div class="col-md-6">
+                                {!! Form::password('password', null, ['class' => 'form-control']) !!}
+                                {!! $errors->first('password', '<p class="help-block">:message</p>') !!}
+                            </div>
+                        </div><div class="form-group {{ $errors->has('password_confirmation') ? 'has-error' : ''}}">
+                            {!! Form::label('password_confirmation', 'Password confirmation', ['class' => 'col-md-4 control-label']) !!}
+                            <div class="col-md-6">
+                                {!! Form::password('password_confirmation', null, ['class' => 'form-control']) !!}
+                                {!! $errors->first('password_confirmation', '<p class="help-block">:message</p>') !!}
+                            </div>
+                        </div>
 
-{!! Form::open(['route' => 'register']) !!}
 
-{!! Form::label('name', 'Name : ') !!}
-{!! Form::text('name') !!}
-<br>
-{!! Form::label('password', 'Password : ') !!}
-{!! Form::password('password') !!}
-<br>
-{!! Form::label('password_confirmation', 'Password confirmation: ') !!}
-{!! Form::password('password_confirmation') !!}
-<br>
-{!! Form::label('email', 'Email : ') !!}
-{!! Form::email('email') !!}
-<br>
-{!! Form::submit('Register !') !!}
-{!! Form::close() !!}
-</div>
+                        <div class="form-group">
+                            <div class="col-md-offset-4 col-md-4">
+                                {!! Form::submit(isset($submitButtonText) ? $submitButtonText : 'Register', ['class' => 'btn btn-primary']) !!}
+                            </div>
+                        </div>
+                        {!! Form::close() !!}
 
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
