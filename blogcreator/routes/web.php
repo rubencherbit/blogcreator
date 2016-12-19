3aecd50cc614f5fbd0f1846b8282c3235b9861ac
laidet_r <rodolphe.laidet@epitech.eu>
2016-12-19 19:23:07 +0100

    [] Add regsiter routes [#]

--- blogcreator/routes/web.php
+++ blogcreator/routes/web.php
@@ -10,3 +10,9 @@
 | to using a Closure or controller method. Build something great!
 |
 */
+
+/*
+ * User routes
+ */
+Route::get('/register', 'Auth\RegisterController@showRegistrationForm');
+Route::post('/register', 'Auth\RegisterController@register');
