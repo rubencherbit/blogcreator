e7f5553f5b1f1572df80042ddbae58583dbc6947
Ruben C <rubencherbit@gmail.com>
2016-12-20 10:09:42 +0100

    controller

--- blogcreator/app/Http/Controllers/CategorieController.php
+++ blogcreator/app/Http/Controllers/CategorieController.php
@@ -2,83 +2,118 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use App\Categorie;
 use Illuminate\Http\Request;
+use Session;
 
 class CategorieController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\View\View
      */
     public function index()
     {
-        //
+        $categorie = Categorie::paginate(25);
+
+        return view('categorie.index', compact('categorie'));
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\View\View
      */
     public function create()
     {
-        //
+        return view('categorie.create');
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
      */
     public function store(Request $request)
     {
-        //
+
+        $requestData = $request->all();
+
+        Categorie::create($requestData);
+
+        Session::flash('flash_message', 'Categorie added!');
+
+        return redirect('categorie');
     }
 
     /**
      * Display the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     *
+     * @return \Illuminate\View\View
      */
     public function show($id)
     {
-        //
+        $categorie = Categorie::findOrFail($id);
+
+        return view('categorie.show', compact('categorie'));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     *
+     * @return \Illuminate\View\View
      */
     public function edit($id)
     {
-        //
+        $categorie = Categorie::findOrFail($id);
+
+        return view('categorie.edit', compact('categorie'));
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
      */
-    public function update(Request $request, $id)
+    public function update($id, Request $request)
     {
-        //
+
+        $requestData = $request->all();
+
+        $categorie = Categorie::findOrFail($id);
+        $categorie->update($requestData);
+
+        Session::flash('flash_message', 'Categorie updated!');
+
+        return redirect('categorie');
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
      */
     public function destroy($id)
     {
-        //
+        Categorie::destroy($id);
+
+        Session::flash('flash_message', 'Categorie deleted!');
+
+        return redirect('categorie');
     }
 }
