a2af3efad281f10d04642f32ac37779006d313b5
Ruben C <rubencherbit@gmail.com>
2016-12-20 11:35:40 +0100

    Comment Controller

--- blogcreator/app/Http/Controllers/CommentController.php
+++ blogcreator/app/Http/Controllers/CommentController.php
@@ -2,83 +2,118 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use App\Comment;
 use Illuminate\Http\Request;
+use Session;
 
 class CommentController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\View\View
      */
     public function index()
     {
-        //
+        $comment = Comment::paginate(25);
+
+        return view('comment.index', compact('comment'));
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\View\View
      */
     public function create()
     {
-        //
+        return view('comment.create');
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
      */
     public function store(Request $request)
     {
-        //
+
+        $requestData = $request->all();
+
+        Comment::create($requestData);
+
+        Session::flash('flash_message', 'Comment added!');
+
+        return redirect('comment');
     }
 
     /**
      * Display the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     *
+     * @return \Illuminate\View\View
      */
     public function show($id)
     {
-        //
+        $comment = Comment::findOrFail($id);
+
+        return view('comment.show', compact('comment'));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     *
+     * @return \Illuminate\View\View
      */
     public function edit($id)
     {
-        //
+        $comment = Comment::findOrFail($id);
+
+        return view('comment.edit', compact('comment'));
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param \Illuminate\Http\Request $request
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
      */
-    public function update(Request $request, $id)
+    public function update($id, Request $request)
     {
-        //
+
+        $requestData = $request->all();
+
+        $comment = Comment::findOrFail($id);
+        $comment->update($requestData);
+
+        Session::flash('flash_message', 'Comment updated!');
+
+        return redirect('comment');
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  int  $id
-     * @return \Illuminate\Http\Response
+     *
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
      */
     public function destroy($id)
     {
-        //
+        Comment::destroy($id);
+
+        Session::flash('flash_message', 'Comment deleted!');
+
+        return redirect('comment');
     }
 }
