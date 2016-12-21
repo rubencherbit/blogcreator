045261d3d43d51d7a86e33de252b643ed492f0a1
laidet_r <rodolphe.laidet@epitech.eu>
2016-12-21 19:35:55 +0100

    [] Display an article in a more convenient way [#]

--- blogcreator/resources/views/article/show.blade.php
+++ blogcreator/resources/views/article/show.blade.php
@@ -5,32 +5,17 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Article {{ $article->id }}</div>
+                    <div class="panel-heading">{{ $article->title }}</div>
                     <div class="panel-body">
-
-                        <a href="{{ url('article/' . $article->id . '/edit') }}" class="btn btn-primary btn-xs" title="Edit Article"><span class="glyphicon glyphicon-pencil" aria-hidden="true"/></a>
-                        {!! Form::open([
-                            'method'=>'DELETE',
-                            'url' => ['article', $article->id],
-                            'style' => 'display:inline'
-                        ]) !!}
-                            {!! Form::button('<span class="glyphicon glyphicon-trash" aria-hidden="true"/>', array(
-                                    'type' => 'submit',
-                                    'class' => 'btn btn-danger btn-xs',
-                                    'title' => 'Delete Article',
-                                    'onclick'=>'return confirm("Confirm delete?")'
-                            ))!!}
-                        {!! Form::close() !!}
-                        <br/>
-                        <br/>
 
                         <div class="table-responsive">
                             <table class="table table-borderless">
                                 <tbody>
-                                    <tr>
-                                        <th>ID</th><td>{{ $article->id }}</td>
-                                    </tr>
-                                    <tr><th> Blog Id </th><td> {{ $article->blog_id }} </td></tr><tr><th> Title </th><td> {{ $article->title }} </td></tr><tr><th> Description </th><td> {{ $article->description }} </td></tr>
+                                    <tr><th> Post date </th><td>{{ $article->created_at }}</td></tr>
+                                    <tr><th> Categorie </th><td> {{ (isset($article->categorie->name)) ? $article->categorie->name : 'No categorie' }} </td></tr>
+                                    <tr><th> Author </th><td> {{ $article->user->name }} </td></tr>
+                                    <tr><th> Description </th><td> {{ $article->description }} </td></tr>
+                                    <tr><th> Content </th><td> {{ $article->content }} </td></tr>
                                 </tbody>
                             </table>
                         </div>
