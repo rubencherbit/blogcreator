08cbae0feb8fc5c91db0ac6fd0bb8759163b0d35
laidet_r <rodolphe.laidet@epitech.eu>
2016-12-22 19:48:06 +0100

    [] Change title of contact page [#]

--- blogcreator/resources/views/message/create.blade.php
+++ blogcreator/resources/views/message/create.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Create New Message</div>
+                    <div class="panel-heading">Send a message to {{ $curr_blog->user->name }}</div>
                     <div class="panel-body">
 
                         @if ($errors->any())
