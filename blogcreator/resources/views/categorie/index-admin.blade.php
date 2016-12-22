798138adaa2f739a33f6c83513b1cd876b7e3d00
laidet_r <rodolphe.laidet@epitech.eu>
2016-12-22 20:18:17 +0100

    [] Custom categorie admin view and remove index route for Categorie ressource [#]

--- blogcreator/resources/views/categorie/index-admin.blade.php
+++ blogcreator/resources/views/categorie/index-admin.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
-                    <div class="panel-heading">Categorie</div>
+                    <div class="panel-heading">Categories</div>
                     <div class="panel-body">
 
                         <a href="{{ url('/categorie/create') }}" class="btn btn-primary btn-xs" title="Add New Categorie"><span class="glyphicon glyphicon-plus" aria-hidden="true"/></a>
@@ -15,16 +15,19 @@
                             <table class="table table-borderless">
                                 <thead>
                                     <tr>
-                                        <th>ID</th><th> Blog Id </th><th> Name </th><th> Description </th><th>Actions</th>
+                                        <th> Blog </th>
+                                        <th> Name </th>
+                                        <th> Description </th>
+                                        <th>Actions</th>
                                     </tr>
                                 </thead>
                                 <tbody>
                                 @foreach($categorie as $item)
                                     <tr>
-                                        <td>{{ $item->id }}</td>
-                                        <td>{{ $item->blog_id }}</td><td>{{ $item->name }}</td><td>{{ $item->description }}</td>
+                                        <td><a href="{{ url('/blogs/' . $item->blog->id) }}">{{ $item->blog->title }}</a></td>
+                                        <td><a href="{{ url('/categorie/' . $item->id) }}">{{ $item->name }}</a></td>
+                                        <td>{{ $item->description }}</td>
                                         <td>
-                                            <a href="{{ url('/categorie/' . $item->id) }}" class="btn btn-success btn-xs" title="View Categorie"><span class="glyphicon glyphicon-eye-open" aria-hidden="true"/></a>
                                             <a href="{{ url('/categorie/' . $item->id . '/edit') }}" class="btn btn-primary btn-xs" title="Edit Categorie"><span class="glyphicon glyphicon-pencil" aria-hidden="true"/></a>
                                             {!! Form::open([
                                                 'method'=>'DELETE',
