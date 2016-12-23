97736816d8fa62a6eaf73cd6c9d2b288d5916712
Ruben C <rubencherbit@gmail.com>
2016-12-23 10:56:25 +0100

    migrate fix

--- blogcreator/database/migrations/2016_12_23_092032_create_share_article.php
+++ blogcreator/database/migrations/2016_12_23_092032_create_share_article.php
@@ -14,7 +14,7 @@ class CreateShareArticle extends Migration
     public function up()
     {
         Schema::create('share_article', function (Blueprint $table) {
-            $table->integer('user_id');
+            $table->integer('blog_id');
             $table->integer('article_id');
         });
     }
